a4300183a0f96269f3646b979df1735cadf8c1df
Europe assistance <charvolin0@gmail.com>
2018-04-16 16:56:43 +0200

    WIP

--- src/rules.js
+++ src/rules.js
@@ -1,4 +1,4 @@
-import { test } from 'ramda';
+import { test, dropLast, drop } from 'ramda';
 
 import { FgRed } from './constants/colors';
 import print from './print';
@@ -19,11 +19,44 @@ const getRuleType = line => {
     }
 };
 
+const getRuleLeftSideLine = line => {
+    let endOfLeftSide = 0;
+    for (var i = 0; i < line.length; i++) {
+        if(line[i] === '<'  || line[i] === '=') break;
+        endOfLeftSide++;
+    }
+    const leftSide = dropLast(line.length - endOfLeftSide, line);
+    if(leftSide.length === 0) {
+        print(`${RULE_FORMAT_ERROR}${line}"`, FgRed);
+        process.exit();
+    }
+    return leftSide;
+};
+
+const getRuleRightSideLine = line => {
+    let endOfLeftSide = 0;
+    for (var i = 0; i < line.length; i++) {
+        endOfLeftSide++;
+        if(line[i] === '>') break;
+    }
+    const rightSide = drop(endOfLeftSide, line);
+    if(rightSide.length === 0) {
+        print(`${RULE_FORMAT_ERROR}${line}"`, FgRed);
+        process.exit();
+    }
+    return rightSide;
+};
+
 export const setRule = (rules, line, lineIndex) => {
     rules = {...rules, [lineIndex]: {
         ...rules[lineIndex],
-        line,
         type: getRuleType(line),
+        leftSide: {
+            line: getRuleLeftSideLine(line)
+        },
+        rightSide: {
+            line: getRuleRightSideLine(line)
+        },
     }}
     return rules;
 }
--- src/solver.js
+++ src/solver.js
@@ -1,9 +1,11 @@
 import { FgGreen } from './constants/colors';
 import print from './print';
 
+const util = require('util')
+
 const solver = dataStruct => {
     print('Solver start', FgGreen)
-    console.log('dataStruct: ', dataStruct);
+    console.log(util.inspect(dataStruct, false, null))
 }
 
 export default solver;
