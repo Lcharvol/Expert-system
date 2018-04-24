7db8d6f20bfcf4d473dfd3da28934fae2d85c906
Lcharvol <charvolin0@gmail.com>
2018-04-24 20:16:41 +0200

    Get affected rules

--- src/solver/index.js
+++ src/solver/index.js
@@ -1,6 +1,7 @@
-import { map } from 'ramda';
+import { map, contains } from 'ramda';
 
 import { FgGreen } from '../constants/colors';
+import { IMPLIES, IF_AND_ONLY_IF } from '../constants/symbols.js';
 import print from '../print';
 import { getSolutionColor } from '../utils';
 import { debugStore } from '../logs';
@@ -16,19 +17,38 @@ const printSolution = dataStruct => {
     },dataStruct.queries)
 };
 
+const getAffectedRules = (querie, rules) => {
+    let affectedRules = [];
+    map(rule => {
+        const { rightSide, leftSide, type } = rule;
+        const { name } = querie;
+        if((contains(name, rightSide.line) && type === IMPLIES) || (contains(name, leftSide.line) && type === IF_AND_ONLY_IF)) {
+            affectedRules = [...affectedRules, rule];
+        }
+    } ,rules)
+    return affectedRules;
+};
+
+const backwardChaining = (querie, dataStruct) => {
+    const { rules } = dataStruct;
+    const affectedRules = getAffectedRules(querie, rules);
+    console.log('affectedRules: ', affectedRules);
+    return dataStruct;
+};
+
 const isInitialFactsContainQuerie = (name, store) => store[name];
 
 const eachQuerie = (querie, dataStruct) => {
     const { store } = dataStruct;
     const { name } = querie;
-    debugStore(dataStruct);
     if(isInitialFactsContainQuerie(name, store)) return dataStruct;
+    dataStruct = backwardChaining(querie, dataStruct);
+    debugStore(dataStruct);
     return dataStruct;
 };
 
 const start = dataStruct => {
     const { queries } = dataStruct;
-    console.log('dataStruct: ', dataStruct)
     print('Initial State: ')
     debugStore(dataStruct);
     map(querie => dataStruct = eachQuerie(querie, dataStruct),queries)
